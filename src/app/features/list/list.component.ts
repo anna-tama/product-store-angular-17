95d9b0f68df748cce8777a76825013091e789dad
Anna.Romanczuk <anna.romanczuk@proveedores.boldt.com.ar>
2024-05-26 17:49:09 -0300

    core: card productos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
+import { MatCardModule} from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatButtonModule],
   providers: [ProductsService],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
